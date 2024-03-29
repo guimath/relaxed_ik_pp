2fbaad38466dc2d21e996c5a258644f56b1a0114
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2024-03-30 01:11:15 +0800

    added python interface

---
+++ relaxed_ik_lib.pyi
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class RelaxedWrapper:
+    """
+    A class representing relaxedIK solver.
+
+    Parameters
+    ----------
+    path_to_setting: path to the setting file (E.G. 'configs/settings.yaml')
+    """
+    def __init__(self, path_to_setting: str) -> None: ...
+
+    def solve_position(self, pos_goals:List[float], quat_goals:List[float], tolerances:List[float]) -> tuple[List[float], float]:
+        """
+        Solves IK for provided pose
+
+        Parameters
+        ----------
+        Assuming the robot has N end-effectors
+        pos_goals: (1D array with length as 3*N) list of end-effector positions
+        quat_goals: (1D array with length as 4*N) list of end-effector orientations (in quaternion xyzw format)
+        tolerances: (1D array with length as 6*N) list of tolerances for each end-effector (x, y, z, rx, ry, rz)
+
+        Returns
+        ----------
+        tuple (joints position at end of optimization, cost)
+        """
+    
+    def reset_origin(self) -> None : 
+        '''
+        Resets joint positions to origin 
+        ''' 
