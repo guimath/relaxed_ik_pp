82958ac21d02aa804550e2efef06e8a7caee266c
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2024-03-29 23:35:21 +0800

    simplified wrapper

--- src/relaxed_ik_wrapper.rs
+++ src/relaxed_ik_wrapper.rs
@@ -1,210 +1,75 @@
-use crate::relaxed_ik::{RelaxedIK, Opt};
-use std::sync::{Arc, Mutex};
-use nalgebra::{Vector3, Vector6, UnitQuaternion, Quaternion,Translation3, Isometry3};
-use std::os::raw::{*};
-use std::str;
-use crate::utils_rust::file_utils::{*};
 
-// http://jakegoulding.com/rust-ffi-omnibus/objects/
-#[no_mangle]
-pub unsafe extern "C" fn relaxed_ik_new(path_to_setting: *const c_char) -> *mut RelaxedIK {
-    if path_to_setting.is_null() 
-    { 
-        let path_to_src = get_path_to_src();
-        let default_path_to_setting = path_to_src +  "configs/settings.yaml";
-        return Box::into_raw(Box::new(RelaxedIK::load_settings(default_path_to_setting.as_str())))
+use pyo3::prelude::*;
+use crate::groove::vars::RelaxedIKVars;
+use crate::groove::objective_master::ObjectiveMaster;
+use crate::groove::groove::OptimizationEngineOpen;
+use nalgebra::{Quaternion, UnitQuaternion, Vector3, Vector6};
+
+#[pyclass]
+pub struct RelaxedWrapper {
+    pub vars: RelaxedIKVars,
+    pub om: ObjectiveMaster,
+    pub groove: OptimizationEngineOpen,
+    pub init_q : Vec<f64>,
+}
+
+#[pymethods]
+impl RelaxedWrapper {
+    #[new]
+    pub fn new(path_to_setting: &str) -> Self {
+        println!("RelaxedIK is using below setting file {}", path_to_setting);
+
+        let vars = RelaxedIKVars::from_local_settings(path_to_setting);
+        let om = ObjectiveMaster::relaxed_ik(&vars.robot.chain_lengths);
+
+        let groove = OptimizationEngineOpen::new(vars.robot.num_dofs.clone());
+        let init_q = vars.init_state.clone();
+        Self{vars, om, groove, init_q}
     }
-    let c_str = std::ffi::CStr::from_ptr(path_to_setting);
-    let path_to_setting_str = c_str.to_str().expect("Not a valid UTF-8 string");
 
-    Box::into_raw(Box::new(RelaxedIK::load_settings(path_to_setting_str)))
-}
+    pub fn solve_position(&mut self, pos_goals:[f64; 3], quat_goals:[f64; 4], tolerance:[f64; 6]) -> (Vec<f64>, Option<f64>){
+        for i in 0..self.vars.robot.num_chains  {//TODO use slices
+            self.vars.goal_positions[i] = Vector3::new(pos_goals[3*i], pos_goals[3*i+1], pos_goals[3*i+2]);
+            let tmp_q = Quaternion::new(quat_goals[4*i+3], quat_goals[4*i], quat_goals[4*i+1], quat_goals[4*i+2]);
+            self.vars.goal_quats[i] =  UnitQuaternion::from_quaternion(tmp_q);
+            self.vars.tolerances[i] = Vector6::new( tolerance[6*i], tolerance[6*i+1], tolerance[6*i+2],
+                tolerance[6*i+3], tolerance[6*i+4], tolerance[6*i+5])
+        }
+                        
+        self.solve()
+    }   
 
-#[no_mangle] 
-pub unsafe extern "C" fn relaxed_ik_free(ptr: *mut RelaxedIK) {
-    if ptr.is_null() { return }
-    Box::from_raw(ptr);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn reset(ptr: *mut RelaxedIK, joint_state: *const c_double, joint_state_length: c_int) {
-    let relaxed_ik = unsafe {
-        assert!(!ptr.is_null());
-        &mut *ptr
-    };
-
-    let x_slice: &[c_double] = std::slice::from_raw_parts(joint_state, joint_state_length as usize);
-    let x_vec = x_slice.to_vec();
-    relaxed_ik.reset(x_vec);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn solve_position(ptr: *mut RelaxedIK, pos_goals: *const c_double, pos_length: c_int, 
-    quat_goals: *const c_double, quat_length: c_int,
-    tolerance: *const c_double, tolerance_length: c_int) -> Opt {
-
-    let relaxed_ik = unsafe {
-        assert!(!ptr.is_null());
-        &mut *ptr
-    };
+    pub fn solve(&mut self) -> (Vec<f64>, Option<f64>) {
+        let mut out_x = self.vars.xopt.clone();
+        let res = self.groove.optimize(&mut out_x, &self.vars, &self.om, 100);
+        let _frames = self.vars.robot.get_frames_immutable(&out_x);
     
-    assert!(!pos_goals.is_null(), "Null pointer for pos goals!");
-    assert!(!quat_goals.is_null(), "Null pointer for quat goals!");
-    assert!(!tolerance.is_null(), "Null pointer for tolerance!"); 
-
-    assert!(pos_length as usize == relaxed_ik.vars.robot.num_chains * 3 , 
-        "Pos vels are expected to have {} numbers, but got {}", 
-        relaxed_ik.vars.robot.num_chains * 3, pos_length);
-    assert!(quat_length as usize == relaxed_ik.vars.robot.num_chains * 4,
-        "Rot vels are expected to have {} numbers, but got {}", 
-        relaxed_ik.vars.robot.num_chains * 4, quat_length);
-    assert!(tolerance_length as usize == relaxed_ik.vars.robot.num_chains * 6, 
-        "Tolerance are expected to have {} numbers, but got {}", 
-        relaxed_ik.vars.robot.num_chains * 6, tolerance_length);
-
-    let pos_slice: &[c_double] = std::slice::from_raw_parts(pos_goals, pos_length as usize);
-    let quat_slice: &[c_double] = std::slice::from_raw_parts(quat_goals, quat_length as usize);
-    let tolerance_slice: &[c_double] = std::slice::from_raw_parts(tolerance, tolerance_length as usize);
-
-    let pos_vec = pos_slice.to_vec();
-    let quat_vec = quat_slice.to_vec();
-    let tolerance_vec = tolerance_slice.to_vec();
-
-    let ja = solve_position_helper(relaxed_ik, pos_vec, quat_vec, tolerance_vec);
-
-    let ptr = ja.as_ptr();
-    let len = ja.len();
-    std::mem::forget(ja);
-    Opt {data: ptr, length: len as c_int}
-}
-
-
-#[no_mangle]
-pub unsafe extern "C" fn solve_velocity(ptr: *mut RelaxedIK, pos_vels: *const c_double, pos_length: c_int, 
-    rot_vels: *const c_double, rot_length: c_int,
-    tolerance: *const c_double, tolerance_length: c_int) -> Opt {
-
-    let relaxed_ik = unsafe {
-        assert!(!ptr.is_null());
-        &mut *ptr
-    };
-    
-    assert!(!pos_vels.is_null(), "Null pointer for pos vels!");
-    assert!(!rot_vels.is_null(), "Null pointer for rot vels!");
-    assert!(!tolerance.is_null(), "Null pointer for tolerance!"); 
-
-    assert!(pos_length as usize == relaxed_ik.vars.robot.num_chains * 3 , 
-        "Pos vels are expected to have {} numbers, but got {}", 
-        relaxed_ik.vars.robot.num_chains * 3, pos_length);
-    assert!(rot_length as usize == relaxed_ik.vars.robot.num_chains * 3,
-        "Rot vels are expected to have {} numbers, but got {}", 
-        relaxed_ik.vars.robot.num_chains * 3, rot_length);
-    assert!(tolerance_length as usize == relaxed_ik.vars.robot.num_chains * 6, 
-        "Tolerance are expected to have {} numbers, but got {}", 
-        relaxed_ik.vars.robot.num_chains * 6, tolerance_length);
-
-    let pos_slice: &[c_double] = std::slice::from_raw_parts(pos_vels, pos_length as usize);
-    let rot_slice: &[c_double] = std::slice::from_raw_parts(rot_vels, rot_length as usize);
-    let tolerance_slice: &[c_double] = std::slice::from_raw_parts(tolerance, tolerance_length as usize);
-
-    let pos_vec = pos_slice.to_vec();
-    let rot_vec = rot_slice.to_vec();
-    let tolerance_vec = tolerance_slice.to_vec();
-
-    let ja = solve_velocity_helper(relaxed_ik, pos_vec, rot_vec, tolerance_vec);
-
-    let ptr = ja.as_ptr();
-    let len = ja.len();
-    std::mem::forget(ja);
-    Opt {data: ptr, length: len as c_int}
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn get_ee_positions(ptr: *mut RelaxedIK) -> Opt {
-    let relaxed_ik = unsafe {
-        assert!(!ptr.is_null());
-        &mut *ptr
-    };
-
-    let mut positions = Vec::new();
-    for i in 0..relaxed_ik.vars.goal_positions.len() {
-        positions.push(relaxed_ik.vars.goal_positions[i].x);
-        positions.push(relaxed_ik.vars.goal_positions[i].y);
-        positions.push(relaxed_ik.vars.goal_positions[i].z);
-    }
-    let ptr = positions.as_ptr();
-    let len = positions.len();
-    std::mem::forget(positions);
-    Opt {data: ptr, length: len as c_int}
-}
-
-// This is mainly for backward compatibility
-#[no_mangle]
-pub unsafe extern "C" fn solve(ptr: *mut RelaxedIK, pos_goals: *const c_double, pos_length: c_int, 
-    quat_goals: *const c_double, quat_length: c_int,
-    tolerance: *const c_double, tolerance_length: c_int) -> Opt {
-
-    let relaxed_ik = unsafe {
-        assert!(!ptr.is_null());
-        &mut *ptr
-    };
-    
-    assert!(!pos_goals.is_null(), "Null pointer for pos goals!");
-    assert!(!quat_goals.is_null(), "Null pointer for quat goals!");
-    assert!(!tolerance.is_null(), "Null pointer for tolerance!"); 
-
-    let pos_slice: &[c_double] = std::slice::from_raw_parts(pos_goals, pos_length as usize);
-    let quat_slice: &[c_double] = std::slice::from_raw_parts(quat_goals, quat_length as usize);
-    let tolerance_slice: &[c_double] = std::slice::from_raw_parts(tolerance, tolerance_length as usize);
-
-    let pos_vec = pos_slice.to_vec();
-    let quat_vec = quat_slice.to_vec();
-    let tolerance_vec = tolerance_slice.to_vec();
-
-    let ja = solve_position_helper(relaxed_ik, pos_vec, quat_vec, tolerance_vec);
-
-    let ptr = ja.as_ptr();
-    let len = ja.len();
-    std::mem::forget(ja);
-    Opt {data: ptr, length: len as c_int}
-}
-
-fn solve_position_helper(relaxed_ik: &mut RelaxedIK, pos_goals: Vec<f64>, quat_goals: Vec<f64>,
-                tolerance: Vec<f64>) -> Vec<f64> {
-
-    for i in 0..relaxed_ik.vars.robot.num_chains  {
-        relaxed_ik.vars.goal_positions[i] = Vector3::new(pos_goals[3*i], pos_goals[3*i+1], pos_goals[3*i+2]);
-        let tmp_q = Quaternion::new(quat_goals[4*i+3], quat_goals[4*i], quat_goals[4*i+1], quat_goals[4*i+2]);
-        relaxed_ik.vars.goal_quats[i] =  UnitQuaternion::from_quaternion(tmp_q);
-        relaxed_ik.vars.tolerances[i] = Vector6::new( tolerance[6*i], tolerance[6*i+1], tolerance[6*i+2],
-            tolerance[6*i+3], tolerance[6*i+4], tolerance[6*i+5])
-    }
-                    
-    let x = relaxed_ik.solve();
-    return x;
-}
-
-fn solve_velocity_helper(relaxed_ik: &mut RelaxedIK, pos_vels: Vec<f64>, rot_vels: Vec<f64>,
-    tolerance: Vec<f64>) -> Vec<f64> {
-
-    for i in 0..relaxed_ik.vars.robot.num_chains  {
-        relaxed_ik.vars.goal_positions[i] += Vector3::new(pos_vels[3*i], pos_vels[3*i+1], pos_vels[3*i+2]);
-        let axisangle = Vector3::new(rot_vels[3*i], rot_vels[3*i+1], rot_vels[3*i+2]);
-        let tmp_q = UnitQuaternion::from_scaled_axis(axisangle);
-        let org_q = relaxed_ik.vars.goal_quats[i].clone();
-        relaxed_ik.vars.goal_quats[i] =  tmp_q * org_q;
-        relaxed_ik.vars.tolerances[i] = Vector6::new( tolerance[3*i], tolerance[3*i+1], tolerance[3*i+2],
-            tolerance[3*i+3], tolerance[3*i+4], tolerance[3*i+5])
+        match res{
+            Ok(status)=> {
+                self.vars.update(out_x.clone());  
+                (out_x, Some(status.cost_value()))
+            },
+            Err(_) => {
+                println!("No valid solution found! Returning previous solution: {:?}. End effector position goals: {:?}", self.vars.xopt, self.vars.goal_positions);
+                (self.vars.xopt.clone(), None)
+            }
+        }
     }
 
-    let x = relaxed_ik.solve();
+    pub fn reset(&mut self, x: Vec<f64>) {
+        self.vars.reset( x.clone());
+    }
 
+    pub fn reset_origin(&mut self){
+        self.vars.reset(self.init_q.clone());
+    }
+}
 
-    let frames = relaxed_ik.vars.robot.get_frames_immutable(&x);
-    let last = frames[0].0.len() - 1 ;
-    let ee_pos = frames[0].0[last];
-    let goal = relaxed_ik.vars.goal_positions[0];
-    let dist = (ee_pos - goal).norm();
-
-    return x;
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn relaxed_ik_lib(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<RelaxedWrapper>()?;
+    Ok(())
 }
