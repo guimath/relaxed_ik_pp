2212b2ef8402c7f195dcc33fb32e7cbbbabfe10d
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2024-09-20 15:30:44 +0800

    combined ik and motion

--- examples/reach.rs
+++ examples/reach.rs
@@ -3,9 +3,7 @@ use nalgebra::{Point2, Point3};
 use relaxed_ik_lib::relaxed_ik_wrapper::RelaxedWrapper;
 use relaxed_ik_lib::utils::config_parser::Config;
 use serde::Serialize;
-use std::path::PathBuf;
-use std::sync::Arc;
-use std::{fs, time};
+use std::{convert::TryInto, path::PathBuf, sync::Arc, fs};
 use urdf_rs::Vec3;
 use urdf_viz::Viewer;
 use urdf_viz::{Action, Key, WindowEvent};
@@ -16,10 +14,9 @@ enum VisMode {
     /// Just show the scene with robot and obstacle
     #[default]
     VisualOnly,
-    /// Show Ik results (movable target)
-    IkOnly,
-    /// Show motion
-    Motion,
+    /// Full simulation (movable target, ik & motion)
+    Full,
+
 }
 
 /// args
@@ -32,7 +29,7 @@ struct Cli {
     #[arg(
         short,
         long,
-        default_value = "configs/ur5_grip.yaml",
+        default_value = "configs/ur5.toml",
         value_name = "FILE PATH"
     )]
     settings: PathBuf,
@@ -50,20 +47,61 @@ struct Cli {
     duration: f64,
 }
 
-const HOW_TO_USE_IK_STR: &str = r#"Up/Down: x change
+const HOW_TO_USE_IK_STR: &str = r#"[Target movement]
+Up/Down: x change
 Left/Right: y change
 h/l: z change
 +/-: Modify increment
-c:  Manual recompute
+
+[actions]
 r:  Reset robot to default
-t:  toggle live recompute
-m: toggle show menu
+c:  ik compute from current pose
+p:  motion compute from current pose
+
+[toggles]
 "#;
 
-const HOW_TO_USE_MOTION_STR: &str = r#"s: start/stop motion
-m: toggle show menu
-"#;
 
+
+fn get_motion(rik: &mut RelaxedWrapper, target:[f64 ; 3], duration:f64) -> Result<Vec<Vec<f64>>, openrr_planner::Error>{
+    let (mut q1, mut q2, _) = rik.grip(target)?;
+    q1.reverse();
+    q2.reverse();
+    let grip_plan: Vec<Vec<f64>> =
+        openrr_planner::interpolate(&q1.clone(), duration, 0.01)
+            .unwrap()
+            .into_iter()
+            .map(|point| point.position)
+            .collect();
+    let mut grip_plan2: Vec<Vec<f64>> = openrr_planner::interpolate(&q2.clone(), 1.0, 0.01)
+        .unwrap()
+        .into_iter()
+        .map(|point| point.position)
+        .collect();
+    grip_plan2.extend(grip_plan);
+    Ok(grip_plan2)
+}
+
+fn get_ik(rik: &mut RelaxedWrapper, target:[f64 ; 3], with_reset:bool, with_approach_dist:bool)-> Vec<f64> {
+    if with_reset {
+        rik.reset_origin();
+    }
+    if with_approach_dist {
+        rik.vars.robot.arms[0].displacements[rik.last_joint_num][2] =
+            rik.gripper_length + rik.config.approach_dist;
+    }
+    let _res = rik.repeat_solve_ik(target); //x: -0.0012 y: -0.1129 z:0.0596
+    // println!(" {res:?}");
+    // let (pos, _quat) = rik.get_ee_pos();
+    // println!(
+    //     "x: {:.4} y: {:.4} z:{:.4}",
+    //     target[0] - pos[0],
+    //     target[1] - pos[1],
+    //     target[2] - pos[2]
+    // );
+    rik.vars.robot.arms[0].displacements[rik.last_joint_num][2] = rik.gripper_length;
+    rik.vars.xopt.clone()
+}
 fn main() {
     env_logger::init();
     let args = Cli::parse();
@@ -82,15 +120,15 @@ fn main() {
     viewer.add_axis_cylinders(&mut window, "test", 0.2);
     // ROBOT
     let description: urdf_rs::Robot =
-        urdf_rs::read_file(rik.config.robot_urdf_path.clone()).expect("robot URDF file not found");
+        urdf_rs::read_file(rik.config.urdf_paths.robot.clone()).expect("robot URDF file not found");
     let robot: Arc<k::Chain<f64>> = Arc::new(k::Chain::from(description.clone()));
-    viewer.add_robot(&mut window, &description, &Default::default());
+    viewer.add_robot(&mut window, &description, &conf.package_paths);
     let robot_viz = &robot;
     robot_viz.update_transforms();
     viewer.update(robot_viz);
     // OBSTACLE
     let new_obst = conf
-        .obstacles_urdf_path
+        .urdf_paths.obstacle
         .expect("No obstacles file in config, add obstacles: FILE_PATH to config")
         .clone();
     let mut obstacle_description = urdf_rs::read_file(new_obst.clone()).unwrap();
@@ -101,10 +139,8 @@ fn main() {
     let menu_pose = Point2::new(10.0f32, 10.0);
     let menu_color = Point3::new(1.0f32, 1.0, 1.0);
     let menu_font = 50.0f32;
-    let info_pose = Point2::new(window.width() as f32 - 40.0, 10.0);
     let info_color = Point3::new(0.0f32, 1.0, 0.0);
     let info_font = 40.0;
-
     match args.mode {
         VisMode::VisualOnly => {
             let q = rik.vars.xopt.clone();
@@ -147,58 +183,79 @@ fn main() {
             }
         }
 
-        VisMode::IkOnly => {
-            println!("{:?}", window.width());
+        VisMode::Full => {
+            // Wether to display control info menu
             let mut show_menu = true;
-            let mut incr = 0.1;
+            // Wether to compute ik with every move 
             let mut live_compute = true;
+            // Wether to reset between each ik (except for manual compute)
+            let mut with_reset = true;
+            // wether to compute ik with approach dist
+            let mut with_approach_dist = false;
+            // target increment
+            let mut incr = 0.1;
+
+
             let mut cur_target = [-100.0f64; 3];
             let mut next_target = target;
-            rik.vars.robot.arms[0].displacements[rik.last_joint_num][2] =
-                rik.gripper_length + rik.config.approach_dist;
+            let mut plans: Vec<Vec<f64>> = Vec::new();
+            let mut last_gripper_pose = [0.0f64; 3];
+
             while window.render_with_camera(&mut viewer.arc_ball) {
                 if cur_target != next_target {
+                    // changed target
                     if live_compute {
-                        let res = rik.solve_ik(next_target); //x: -0.0012 y: -0.1129 z:0.0596
-                        println!(" {res:?}");
-                        let (pos, _quat) = rik.get_ee_pos();
-                        println!(
-                            "x: {:.4} y: {:.4} z:{:.4}",
-                            target[0] - pos[0],
-                            target[1] - pos[1],
-                            target[2] - pos[2]
-                        );
-                        robot.set_joint_positions_clamped(&rik.vars.xopt.clone());
+                        plans.push(get_ik(&mut rik,next_target, with_reset, with_approach_dist));
                         cur_target = next_target;
                     }
-
+                    // moving obstacle
                     viewer.remove_robot(&mut window, &obstacle_description);
                     obstacle_description.links[0].visual[0].origin.xyz = Vec3(next_target);
                     obstacle_description.links[0].collision[0].origin.xyz = Vec3(next_target);
                     viewer.add_robot(&mut window, &obstacle_description, &Default::default());
                 }
+                
+                if !plans.is_empty() {
+                    let plan = plans.pop().unwrap();
+                    robot.set_joint_positions_clamped(&plan);
+                    viewer.update(robot_viz);
+                    let pose = rik
+                        .vars
+                        .robot
+                        .get_ee_pos_and_quat_immutable(&plan);
+                    last_gripper_pose = pose[0].0.as_slice().try_into().unwrap();
+                    std::thread::sleep(std::time::Duration::from_millis(10));
+                }
+
                 if show_menu {
+                    let lc_status =  if live_compute {"on"} else {"off"};
+                    let reset_status =  if with_reset {"on"} else {"off"};
+                    let pre_grasp_status =  if with_approach_dist {"on"} else {"off"};
+                    let how_to_use = 
+                        HOW_TO_USE_IK_STR.to_string() +
+                        format!("t: live compute ({lc_status})\nw: reset between ik ({reset_status})\ng: pre-grasp ik ({pre_grasp_status})\nm: menu (on)").as_str();
+
+                    let info_pose = Point2::new(window.width() as f32 * 2.0 - 600.0, 10.0);
                     viewer.draw_text(
                         &mut window,
-                        HOW_TO_USE_IK_STR,
+                        how_to_use.as_str(),
                         menu_font,
                         &menu_pose,
                         &menu_color,
                     );
+                    viewer.draw_text(
+                        &mut window,
+                        format!("target : {:.3} {:.3} {:.3}\nDelta : {:.3} {:.3} {:.3}",
+                            next_target[0], next_target[1], next_target[2],
+                            last_gripper_pose[0] - next_target[0], last_gripper_pose[1] - next_target[1], last_gripper_pose[2] - next_target[2],
+                        )
+                            .as_str(),
+                        info_font,
+                        &info_pose,
+                        &info_color,
+                    );
                 }
-                viewer.draw_text(
-                    &mut window,
-                    format!(
-                        "target : {:.3} {:.3} {:.3}",
-                        next_target[0], next_target[1], next_target[2]
-                    )
-                    .as_str(),
-                    info_font,
-                    &info_pose,
-                    &info_color,
-                );
-
-                viewer.update(robot_viz);
+                
                 for event in window.events().iter() {
                     if let WindowEvent::Key(code, Action::Press, _mods) = event.value {
                         match code {
@@ -210,90 +267,29 @@ fn main() {
                             Key::L => next_target[2] -= incr,
                             Key::Add => incr += incr / 10.0,
                             Key::Subtract => incr -= incr / 10.0,
-                            Key::C => cur_target[0] += 1.0, // just to trigger recompute
+                            Key::P => {
+                                match get_motion(&mut rik,next_target, args.duration) {
+                                    Ok(plan) => {
+                                        plans.extend(plan);
+                                        cur_target = next_target;
+                                    }
+                                    Err(e) => println!("Error when calc motion {e}")
+                                }
+                                
+                            }
+                            Key::C => {
+                                plans.push(get_ik(&mut rik,next_target, false, with_approach_dist));
+                                cur_target = next_target;
+                            }
                             Key::R => {
                                 rik.reset_origin();
-                                robot.set_joint_positions_clamped(&rik.vars.xopt.clone());
+                                plans.push(rik.vars.xopt.clone());
                             }
                             Key::T => live_compute = !live_compute,
                             Key::M => show_menu = !show_menu,
+                            Key::W => with_reset = !with_reset,
+                            Key::G => with_approach_dist = !with_approach_dist,
                             a => println!("{a:?}"),
-                        }
-                    }
-                }
-            }
-        }
-
-        VisMode::Motion => {
-            let mut show_menu = true;
-            let t1 = time::Instant::now();
-            let (q1, q2) = match rik.grip(target) {
-                Ok((x1, x2, _)) => (x1, x2),
-                Err(e) => {
-                    println!("error {e:}");
-                    (
-                        vec![conf.starting_config.clone(), conf.starting_config.clone()],
-                        vec![conf.starting_config.clone(), conf.starting_config.clone()],
-                    )
-                } // To show env even if failed
-            };
-
-            let elapsed = time::Instant::now() - t1;
-            println!("Time elapsed = {elapsed:.3?}");
-
-            let mut q = q1.clone();
-            q.extend(q2.clone());
-            let mut extra_wait = 100usize;
-
-            if args.full_move {
-                // -> interpolate
-                let mut grip_plan: Vec<Vec<f64>> =
-                    openrr_planner::interpolate(&q1.clone(), args.duration, 0.01)
-                        .unwrap()
-                        .into_iter()
-                        .map(|point| point.position)
-                        .collect();
-                let grip_plan2: Vec<Vec<f64>> = openrr_planner::interpolate(&q2.clone(), 1.0, 0.01)
-                    .unwrap()
-                    .into_iter()
-                    .map(|point| point.position)
-                    .collect();
-                grip_plan.extend(grip_plan2);
-                q = grip_plan.clone();
-                extra_wait = 1;
-            }
-
-            let mut incr = 1;
-            let tot = q.len();
-            let (mut i, mut j) = (0usize, 0usize);
-            while window.render_with_camera(&mut viewer.arc_ball) {
-                if show_menu {
-                    viewer.draw_text(
-                        &mut window,
-                        HOW_TO_USE_MOTION_STR,
-                        menu_font,
-                        &menu_pose,
-                        &menu_color,
-                    );
-                }
-                robot.set_joint_positions_clamped(&q[i]);
-                viewer.update(robot_viz);
-                j = (j + incr) % extra_wait;
-                if j == 0 {
-                    i = (i + incr) % tot;
-                }
-                std::thread::sleep(std::time::Duration::from_millis(10));
-                for event in window.events().iter() {
-                    if let WindowEvent::Key(code, Action::Press, _mods) = event.value {
-                        match code {
-                            Key::S => {
-                                incr = (incr + 1) % 2;
-                                println!("Current pos : {:?}", q[i]);
-                            }
-                            Key::M => {
-                                show_menu = !show_menu;
-                            }
-                            _ => (),
                         }
                     }
                 }
