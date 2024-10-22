ccb7e2fe67119ca77607eaa7c4829cfd86f21b20
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2024-10-22 16:50:06 +0800

    improved error messaging on specific links

--- src/spacetime/robot.rs
+++ src/spacetime/robot.rs
@@ -20,7 +20,6 @@ impl Robot {
         // let chain = k::Chain::<f64>::from_urdf_file(urdf).unwrap();
         let description: urdf_rs::Robot = urdf_rs::read_from_string(urdf).unwrap();
         let chain: k::Chain<f64> = k::Chain::from(description.clone());
-
         let mut arms: Vec<RevoluteArm> = Vec::new();
         let num_chains = base_links.len();
         let mut num_dof = 0;
@@ -29,8 +28,8 @@ impl Robot {
         let mut lower_joint_limits: Vec<f64> = Vec::new();
 
         for i in 0..num_chains {
-            let base_link = chain.find_link(base_links[i].as_str()).unwrap();
-            let ee_link = chain.find_link(ee_links[i].as_str()).unwrap();
+            let base_link = chain.find_link(base_links[i].as_str()).expect(format!("Base link \"{}\" was not found in robot urdf", base_links[i]).as_str());
+            let ee_link = chain.find_link(ee_links[i].as_str()).expect(format!("End effector link \"{}\" was not found in robot urdf", ee_links[i]).as_str());
             let serial_chain = k::SerialChain::from_end_to_root(ee_link, base_link);
             let arm= RevoluteArm::from_chain(serial_chain);
             num_dof += arm.num_dof;
